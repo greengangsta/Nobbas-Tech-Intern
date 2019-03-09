646e08b0b32d024c1a8aeefcf2b7607a0361d97f
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-03-09 20:00:41 +0530

    importing the libraries and dataset

--- soln.py
+++ soln.py
@@ -1,1 +1,12 @@
+# Importing the libraries
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Loading the dataset
+
+X = pd.read_csv('CSV 1.csv')
+X2 = pd.read_csv('CSV 2.csv')
+X3 = pd.read_csv('CSV 3.csv')
